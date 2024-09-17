825c632fabbdac11eb6c90d1e82e8d72634a0270
Isaque Lis <isaquelis.g@gmail>
2024-09-17 16:25:48 -0300

    new: controllers-set

---
+++ src/main/java/edu/ifbasaj/pweb/cadastro_de_funcionarios/controller/FuncionarioController.java
@@ -0,0 +1,10 @@
+package edu.ifbasaj.pweb.cadastro_de_funcionarios.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"/crud"})
+public class FuncionarioController {
+    
+}
---
+++ src/main/java/edu/ifbasaj/pweb/cadastro_de_funcionarios/controller/FuncionarioRestControllerAPI.java
@@ -0,0 +1,10 @@
+package edu.ifbasaj.pweb.cadastro_de_funcionarios.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping({"/api"})
+public class FuncionarioRestControllerAPI {
+    
+}
