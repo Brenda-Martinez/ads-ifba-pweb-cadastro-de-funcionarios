18c295a912e409df05bac63b10c1a8883377f5b2
Isaque Lis <isaquelis.g@gmail>
2024-09-25 19:16:31 -0300

    new: Página de Endereço

---
+++ src/main/java/edu/ifbasaj/pweb/cadastro_de_funcionarios/endereco/controller/EnderecoController.java
@@ -0,0 +1,33 @@
+package edu.ifbasaj.pweb.cadastro_de_funcionarios.endereco.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import edu.ifbasaj.pweb.cadastro_de_funcionarios.endereco.service.EnderecoService;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequestMapping("endereco")
+@RequiredArgsConstructor
+public class EnderecoController {
+    
+    private final EnderecoService enderecoService;
+
+    @GetMapping("visualizar_endereco")
+    String getPageGerenciarFuncionario(@RequestParam(value = "id", required = false) String id, Model model){
+        
+        if(id == null){
+            var enderecoLista = enderecoService.findAll().block();
+            model.addAttribute("enderecos", enderecoLista);
+        } else {
+            var enderecoLista = enderecoService.findById(Long.parseLong(id)).block();
+            model.addAttribute("enderecos", enderecoLista);
+        }
+
+        return "endereco/visualizar_endereco";
+    }
+
+}
