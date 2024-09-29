cdf4468abe9ed63ea8a78f6965dce3a3cf3bd37a
Isaque Lis <isaquelis.g@gmail>
2024-09-28 21:01:36 -0300

    mod: versão prévia do front (wireframe) + última release do backend

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -1,5 +1,24 @@
 
 
-function closeMe(element){
-    element.parentElement.remove();
+function showNavOptions(sectionId){
+
+    const section = document.getElementById(sectionId);
+    
+    if(section.classList.contains("fun-nav-hidden")){
+        section.classList.remove("fun-nav-hidden");
+        section.classList.add("fun-nav-shown");
+    } else {
+        section.classList.remove("fun-nav-shown");
+        section.classList.add("fun-nav-hidden");
+    }
+
+    const showedSections = document.querySelectorAll(".fun-nav-shown");
+
+    showedSections.forEach( sect => {
+
+        if(sect != section){
+            sect.classList.remove("fun-nav-shown");
+            sect.classList.add("fun-nav-hidden");
+        }
+    });
 }
